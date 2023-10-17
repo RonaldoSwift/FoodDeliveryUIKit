43619271bc7b984fa8edf87f9b9d597f472e3273
CarlitosDroid <carlosleonardovargashuaman@gmail.com>
2023-10-17 13:36:36 -0500

    Updated design animation

--- FoodDeliveryUIKit/Presentation/OnBoarding/OnBoardingViewController.swift
+++ FoodDeliveryUIKit/Presentation/OnBoarding/OnBoardingViewController.swift
@@ -11,11 +11,14 @@ class OnBoardingViewController: UIViewController {
     
     @IBOutlet weak var nextButton: UIButton!
     @IBOutlet weak var pageControl: UIPageControl!
-    
     @IBOutlet weak var onboardingCollectionView: UICollectionView!
     
     
-    var slides: [OnboardingSlide] = []
+    var slides: [OnboardingSlide] = [
+        OnboardingSlide(image: #imageLiteral(resourceName: "Onboarding1"), title: "Wide range of Food Categories & more", description: "Browse through our extensive list of restaurants and dishes, and when you're ready to order, simply add your desired items to your cart and checkout. It's that easy!"),
+        OnboardingSlide(image: #imageLiteral(resourceName: "Onboarding2"), title: "Free Deliveries for ONE MONTH!!!", description: "Get your favorite meals delivered to your doorstep for free with our online food delivery app - enjor a whole month of complimentary delivery!"),
+        OnboardingSlide(image: #imageLiteral(resourceName: "Onboarding3"), title: "Get started on Ordering your Food", description: "Please create an account or sign in to your existing account to start borwsing our selection of delicious meals from your favorite restaurants."),
+    ]
     
     var currentPage = 0 {
         didSet {
@@ -30,51 +33,32 @@ class OnBoardingViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        slides = [
-            OnboardingSlide(image: #imageLiteral(resourceName: "Onboarding1"), title: "Wide range of Food Categories & more", description: "Browse through our extensive list of restaurants and dishes, and when you're ready to order, simply add your desired items to your cart and checkout. It's that easy!"),
-            OnboardingSlide(image: #imageLiteral(resourceName: "Onboarding2"), title: "Free Deliveries for ONE MONTH!!!", description: "Get your favorite meals delivered to your doorstep for free with our online food delivery app - enjor a whole month of complimentary delivery!"),
-            OnboardingSlide(image: #imageLiteral(resourceName: "Onboarding3"), title: "Get started on Ordering your Food", description: "Please create an account or sign in to your existing account to start borwsing our selection of delicious meals from your favorite restaurants."),
-        ]
-        
-        
         setUpViews()
-        
-        // Do any additional setup after loading the view.
     }
     
     private func setUpViews() {
+        pageControl.numberOfPages = slides.count
         nextButton.setPadding(top: 20, leading: 60, bottom: 20, trailing: 60)
     }
     
-    
-    
     @IBAction func onClickNextButton(_ sender: Any) {
         
-        if (currentPage == slides.count - 1) {
-            print("navigate to next screen")
+        if currentPage == slides.count - 1 {
+            print("navogate")
+            performSegue(withIdentifier: "AuthenticationSegue", sender: nil)
         } else {
             currentPage += 1
             let indexPath = IndexPath(item: currentPage, section: 0)
+            onboardingCollectionView.isPagingEnabled = false
             onboardingCollectionView.scrollToItem(
                 at: indexPath,
-                at: .left,
+                at: .centeredHorizontally,
                 animated: true
             )
+            onboardingCollectionView.isPagingEnabled = true
         }
         
     }
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
 }
 
 extension OnBoardingViewController: UICollectionViewDelegate, UICollectionViewDataSource {
@@ -92,15 +76,11 @@ extension OnBoardingViewController: UICollectionViewDelegate, UICollectionViewDa
 extension OnBoardingViewController: UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(
-            width: collectionView.frame.size.width,
-            height: collectionView.frame.size.height
-        )
+        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
     }
     
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
         let width = scrollView.frame.width
         currentPage = Int(scrollView.contentOffset.x / width)
-        //pageControl.currentPage = currentPage
     }
 }
