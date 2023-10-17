9448fd0477332703123ffd57a60b0c6f7194c6c5
CarlitosDroid <carlosleonardovargashuaman@gmail.com>
2023-10-17 14:37:24 -0500

    Added ViewModel

--- FoodDeliveryUIKit/Presentation/OnBoarding/OnBoardingViewController.swift
+++ FoodDeliveryUIKit/Presentation/OnBoarding/OnBoardingViewController.swift
@@ -6,24 +6,24 @@
 //
 
 import UIKit
+import Combine
 
 class OnBoardingViewController: UIViewController {
+    
+    // MARK: - View Model
+    var viewModel = OnboardingViewModel()
     
     @IBOutlet weak var nextButton: UIButton!
     @IBOutlet weak var pageControl: UIPageControl!
     @IBOutlet weak var onboardingCollectionView: UICollectionView!
     
-    
-    var slides: [OnboardingSlide] = [
-        OnboardingSlide(image: #imageLiteral(resourceName: "Onboarding1"), title: "Wide range of Food Categories & more", description: "Browse through our extensive list of restaurants and dishes, and when you're ready to order, simply add your desired items to your cart and checkout. It's that easy!"),
-        OnboardingSlide(image: #imageLiteral(resourceName: "Onboarding2"), title: "Free Deliveries for ONE MONTH!!!", description: "Get your favorite meals delivered to your doorstep for free with our online food delivery app - enjor a whole month of complimentary delivery!"),
-        OnboardingSlide(image: #imageLiteral(resourceName: "Onboarding3"), title: "Get started on Ordering your Food", description: "Please create an account or sign in to your existing account to start borwsing our selection of delicious meals from your favorite restaurants."),
-    ]
+    var slides: [OnboardingSlide] = []
+    private var subscriptions = Set<AnyCancellable>()
     
     var currentPage = 0 {
         didSet {
             pageControl.currentPage = currentPage
-            if currentPage == slides.count - 1 {
+            if currentPage == viewModel.slides.count - 1 {
                 nextButton.setTitle("Continue", for: .normal)
             } else {
                 nextButton.setTitle("Next", for: .normal)
@@ -31,20 +31,34 @@ class OnBoardingViewController: UIViewController {
         }
     }
     
+    // MARK: - View Controller's Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpViews()
+        setupBindings()
     }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+    
+    // MARK: - Setup Methods
     private func setUpViews() {
-        pageControl.numberOfPages = slides.count
         nextButton.setPadding(top: 20, leading: 60, bottom: 20, trailing: 60)
+    }
+    
+    private func setupBindings() {
+        viewModel.$slides.sink { (slides: [OnboardingSlide]) in
+            self.slides = slides
+            self.pageControl.numberOfPages = slides.count
+            self.onboardingCollectionView.reloadData()
+        }.store(in: &subscriptions)
     }
     
     @IBAction func onClickNextButton(_ sender: Any) {
         
-        if currentPage == slides.count - 1 {
-            print("navogate")
+        if currentPage == viewModel.slides.count - 1 {
             performSegue(withIdentifier: "AuthenticationSegue", sender: nil)
         } else {
             currentPage += 1
@@ -61,6 +75,8 @@ class OnBoardingViewController: UIViewController {
     }
 }
 
+// MARK: - CollectionView Delegates
+
 extension OnBoardingViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return slides.count
@@ -68,11 +84,12 @@ extension OnBoardingViewController: UICollectionViewDelegate, UICollectionViewDa
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: OnboardingCollectionViewCell.identifier, for: indexPath) as! OnboardingCollectionViewCell
-        cell.setup(slides[indexPath.row])
+        cell.setup(viewModel.slides[indexPath.row])
         return cell
     }
 }
 
+// MARK: - CollectionView FlowLayout Delegate
 extension OnBoardingViewController: UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
