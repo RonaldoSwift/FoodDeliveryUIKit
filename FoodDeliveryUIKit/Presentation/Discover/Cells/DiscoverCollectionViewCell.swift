cf0234ba8a1efa556a303ab660a95dfb50f3dbd1
CarlitosDroid <carlosleonardovargashuaman@gmail.com>
2023-10-28 12:58:03 -0500

    Added Discover screen

---
+++ FoodDeliveryUIKit/Presentation/Discover/Cells/DiscoverCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  DiscoverCollectionViewCell.swift
+//  FoodDeliveryUIKit
+//
+//  Created by Carlos Leonardo Camilo Vargas Huaman on 28/10/23.
+//
+
+import UIKit
+
+class DiscoverCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
