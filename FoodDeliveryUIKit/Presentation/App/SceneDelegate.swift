b72ca5137b2c17a00625d8a59ebd60db9e4901bd
Ronado Vargas <ronaldovh99@gmail.com>
2023-11-02 10:56:47 -0500

    Modicado ScenaDelegate

--- FoodDeliveryUIKit/Presentation/App/SceneDelegate.swift
+++ FoodDeliveryUIKit/Presentation/App/SceneDelegate.swift
@@ -19,12 +19,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowsScena = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowsScena)
-        let storyBoard = UIStoryboard(name: "Gallery", bundle: nil)
+        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
         
         if(UserDefaults.standard.bool(forKey: "logeado")){
             window.rootViewController = storyBoard.instantiateViewController(withIdentifier: "PrincipalTabBarController")
         } else{
-            window.rootViewController = storyBoard.instantiateViewController(withIdentifier: "GalleryViewController")
+            window.rootViewController = storyBoard.instantiateViewController(withIdentifier: "LoginNavigationController")
         }
         self.window = window
         window.makeKeyAndVisible()
